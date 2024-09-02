1941e09c548c352a316d580d45bb88229756fec3
Andrey <andreeey.gg@gmail.com>
2024-09-02 15:48:41 +0200

    add description and rating

--- app/src/main/java/com/example/ecommerce_android_app/models/NavCategoryDetailedModel.java
+++ app/src/main/java/com/example/ecommerce_android_app/models/NavCategoryDetailedModel.java
@@ -1,17 +1,24 @@
 package com.example.ecommerce_android_app.models;
 
 
-public class NavCategoryDetailedModel {
+import java.io.Serializable;
+
+public class NavCategoryDetailedModel implements Serializable {
     String name;
-    String type;
+    String description;
+    String rating;
     String img_url;
+    String type;
     String price;
 
-    public NavCategoryDetailedModel(String name, String type,
-                                    String img_url, String price) {
+    public NavCategoryDetailedModel(String name, String description,
+                                    String rating, String img_url,
+                                    String type, String price) {
         this.name = name;
-        this.type = type;
+        this.description = description;
+        this.rating = rating;
         this.img_url = img_url;
+        this.type = type;
         this.price = price;
     }
 
@@ -26,12 +33,20 @@ public class NavCategoryDetailedModel {
         this.name = name;
     }
 
-    public String getType() {
-        return type;
+    public String getDescription() {
+        return description;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
     }
 
     public String getImg_url() {
@@ -40,6 +55,14 @@ public class NavCategoryDetailedModel {
 
     public void setImg_url(String img_url) {
         this.img_url = img_url;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
     public String getPrice() {
